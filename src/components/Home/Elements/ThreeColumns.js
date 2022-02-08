8e410b0976afe6208c0a99605b5207fa16e350c7
andro2ss <a.brzeski@hotmail.com>
2022-02-08 07:07:46 -0800

    second home section - done

---
+++ src/components/Home/Elements/ThreeColumns.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export function ThreeColumns() {
+  return (
+    <div id="threeColumns">
+      <div className="column__container">
+        <span>10</span>
+        <span>ODDANYCH&nbsp;WORKÓW</span>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi
+          repellat, veniam. Dolore eum incidunt magni maxime natus repellendus
+          similique temporibus!
+        </span>
+      </div>
+      <div className="column__container">
+        <span>5</span>
+        <span>WSPARTYCH&nbsp;ORGANIZACJI</span>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
+          cupiditate facilis ipsam quis sint, voluptates!
+        </span>
+      </div>
+      <div className="column__container">
+        <span>7</span>
+        <span>ZORGANIZOWANYCH&nbsp;ZBIÓREK</span>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi
+          repellat, veniam. Dolore eum incidunt magni maxime natus repellendus
+          similique temporibus!
+        </span>
+      </div>
+    </div>
+  );
+}
