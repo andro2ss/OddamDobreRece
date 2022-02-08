4d030b37ccbeb2f89d4911cc4bdd517d849e4d16
andro2ss <a.brzeski@hotmail.com>
2022-02-08 07:04:15 -0800

    home page done start and threeColumns sections

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Start } from "./Elements/Start";
+import { ThreeColumns } from "./Elements/ThreeColumns";
+
+function Home(props) {
+  return (
+    <>
+      <Start />
+      <ThreeColumns />
+    </>
+  );
+}
+
+export default Home;
