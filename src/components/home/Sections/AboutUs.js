27730367f1903e2da5e2b65c5daa34af96a2e46d
andro2ss <a.brzeski@hotmail.com>
2022-02-09 04:19:20 -0800

    added section About Us

---
+++ src/components/home/Sections/AboutUs.js
@@ -0,0 +1,30 @@
+import { Decoration } from "../../common/decorations/Decoration";
+import React from "react";
+
+export function AboutUs() {
+  return (
+    <div id="aboutUs">
+      <div className="container--left">
+        <h2 className="section__title">O nas</h2>
+        <Decoration />
+        <span className="text">
+          Nori grape silver beet broccoli kombu beet greens fava bean potato
+          quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil
+          turnip greens parsnip.
+        </span>
+        <img
+          className="img--sign"
+          src={require("../../../assets/Signature@2x.png")}
+          alt="podpis"
+        />
+      </div>
+      <div className="img__container">
+        <img
+          className="img--people"
+          src={require("../../../assets/People.jpg")}
+          alt="ludzie"
+        />
+      </div>
+    </div>
+  );
+}
