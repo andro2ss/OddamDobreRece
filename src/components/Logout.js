2195103521f61d860ca0284074c5e6906f904434
andro2ss <a.brzeski@hotmail.com>
2022-02-11 11:30:56 -0800

    styled pages register login logout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,21 +2,19 @@ import React from "react";
 import { Decoration } from "./common/decorations/Decoration";
 import { Link } from "react-router-dom";
 
-function Logout(props) {
+function Logout() {
   return (
     <div className="account__container">
       <h2 className="section__title">
-        {" "}
         Wylogowanie nastąpiło
         <br />
         pomyślnie!
       </h2>
       <Decoration />
       <div className="btn__box">
-        {" "}
-        <Link to="../" className="btn__account frame">
-          Strona główna{" "}
-        </Link>{" "}
+        <Link to="../" className="btn__account frame center">
+          Strona główna
+        </Link>
       </div>
     </div>
   );
