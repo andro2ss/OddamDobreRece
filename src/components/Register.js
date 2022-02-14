427e09cc564d10a468df0daac93cfaa6a29be1f4
andro2ss <a.brzeski@hotmail.com>
2022-02-14 06:28:39 -0800

    validation progress backup

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Decoration } from "./common/decorations/Decoration";
 import { Link } from "react-router-dom";
+import { errorHandler } from "../functions/accountSection/ErrorHandler";
 
 function Register(props) {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [error, setError] = useState([0, 0, 0]);
+  const errorsHandler = errorHandler(
+    error,
+    login,
+    password,
+    password2,
+    setError
+  );
+
   return (
     <div className="account__container">
       <h2 className="section__title"> Załóż konto</h2>
       <Decoration />
-      <form className="account__form">
+      <form
+        className="account__form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          errorsHandler();
+        }}
+      >
+        <button style={{ display: "none" }}>send</button>
         <div className="box__account">
           <label htmlFor="register__email">Email</label>
           <input
