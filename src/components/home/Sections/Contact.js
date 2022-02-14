38ea17c0f044499615f499467fae9de62e90a6d0
andro2ss <a.brzeski@hotmail.com>
2022-02-14 05:19:13 -0800

    moved out functions

--- src/components/home/Sections/Contact.js
+++ src/components/home/Sections/Contact.js
@@ -1,6 +1,11 @@
 import { Decoration } from "../../common/decorations/Decoration";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import { sendForms } from "../../../functions/SendForms";
+import { validationEmail } from "../../../functions/ValidationEmail";
+import { validationName } from "../../../functions/ValidationName";
+import { validationMessage } from "../../../functions/ValidationMessage";
+import LinearIndeterminate from "../../common/Spinners";
+import { handleSubmitBtn } from "../../../functions/HandleSubmitBtn";
 
 export function Contact() {
   const [name, setName] = useState("");
@@ -11,68 +16,23 @@ export function Contact() {
     email: "nok",
     message: "nok",
   });
+  const [spinner, setSpinner] = useState(0);
 
-  const validateName = (name) => {
-    let tempStatus = status;
-    const domObject = document.getElementById("form__name");
-    if (name.split(" ").length > 1) {
-      tempStatus.name = "error";
-      domObject.classList.add("error");
-      domObject.parentElement.lastElementChild.classList.add("visible");
-    } else if (!name) {
-      tempStatus.name = "nok";
-      domObject.classList.remove("error");
-      domObject.parentElement.lastElementChild.classList.remove("visible");
-    } else {
-      tempStatus.name = "ok";
-      domObject.classList.remove("error");
-      domObject.parentElement.lastElementChild.classList.remove("visible");
-    }
-    setStatus(tempStatus);
-  };
-
-  const validateMessage = (message) => {
-    let tempStatus = status;
-    let messageLength = message.split("").length;
-    const domObject = document.getElementById("form__message");
-    if (1 <= messageLength && messageLength < 120) {
-      tempStatus.message = "error";
-      domObject.classList.add("error");
-      domObject.parentElement.lastElementChild.classList.add("visible");
-    } else if (!message) {
-      tempStatus.message = "nok";
-      domObject.classList.remove("error");
-      domObject.parentElement.lastElementChild.classList.remove("visible");
-    } else {
-      tempStatus.message = "ok";
-      domObject.classList.remove("error");
-      domObject.parentElement.lastElementChild.classList.remove("visible");
-    }
-    setStatus(tempStatus);
-  };
-
-  const validateEmail = (email) => {
-    const emailCheck =
-      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
-    let tempStatus = status;
-    const domObject = document.getElementById("form__email");
-    if (!emailCheck.test(email) && email.length) {
-      tempStatus.email = "error";
-      domObject.classList.add("error");
-      domObject.parentElement.lastElementChild.classList.add("visible");
-    } else if (!email) {
-      tempStatus.email = "nok";
-      domObject.classList.remove("error");
-      domObject.parentElement.lastElementChild.classList.remove("visible");
-    } else {
-      tempStatus.email = "ok";
-      domObject.classList.remove("error");
-      domObject.parentElement.lastElementChild.classList.remove("visible");
-    }
-    setStatus(tempStatus);
-  };
-
-  useEffect(() => {}, [name, email, message]);
+  const validateName = validationName(status, setStatus);
+  const validateMessage = validationMessage(status, setStatus);
+  const validateEmail = validationEmail(status, setStatus);
+  const sendForm = sendForms(setName, setEmail, setMessage, setSpinner);
+  const handleSubmit = handleSubmitBtn(
+    validateName,
+    name,
+    validateEmail,
+    email,
+    validateMessage,
+    message,
+    status,
+    setSpinner,
+    sendForm
+  );
 
   return (
     <div id="contact">
@@ -81,58 +41,7 @@ export function Contact() {
           className="form"
           onSubmit={(e) => {
             e.preventDefault();
-            validateName(name);
-            validateEmail(email);
-            validateMessage(message);
-            if (status.name !== "ok") {
-              let domObject = document.getElementById("form__name");
-              domObject.classList.add("error");
-              domObject.parentElement.lastElementChild.classList.add("visible");
-            }
-            if (status.email !== "ok") {
-              let domObject = document.getElementById("form__email");
-              domObject.classList.add("error");
-              domObject.parentElement.lastElementChild.classList.add("visible");
-            }
-            if (status.message !== "ok") {
-              let domObject = document.getElementById("form__message");
-              domObject.classList.add("error");
-              domObject.parentElement.lastElementChild.classList.add("visible");
-            }
-            if (
-              status.message === "ok" &&
-              status.name === "ok" &&
-              status.email === "ok"
-            ) {
-              let axisConfig = {
-                headers: { "Content-Type": "application/json" },
-              };
-              const postData = {
-                name: name,
-                email: email,
-                message: message,
-              };
-              axios
-                .post(
-                  "https://fer-api.coderslab.pl/v1/portfolio/contact",
-                  postData,
-                  axisConfig
-                )
-                .then((res) => {
-                  console.log("RESPONSE RECEIVED: ", res);
-                  document
-                    .querySelector(".success__message")
-                    .classList.add("visible");
-                })
-                .catch((err) => {
-                  console.log("AXIOS ERROR: ", err);
-                  alert("Coś poszło nie tak, spróbuj ponownie później.");
-                });
-            } else {
-              document
-                .querySelector(".success__message")
-                .classList.remove("visible");
-            }
+            handleSubmit();
           }}
         >
           <h2 className="section__title">Skontaktuj się z nami</h2>
@@ -150,6 +59,7 @@ export function Contact() {
                 id="form__name"
                 name="form__name"
                 placeholder="Krzysztof"
+                value={name}
                 onChange={(e) => {
                   setName(e.target.value);
                 }}
@@ -162,7 +72,7 @@ export function Contact() {
             <div className="input__box">
               <label htmlFor="form__email">Wpisz swój email</label>
               <input
-                // type="email"
+                value={email}
                 id="form__email"
                 name="form__email"
                 placeholder="abc@xyz.pl"
@@ -180,6 +90,7 @@ export function Contact() {
             <label htmlFor="form__message">Wpisz swoją wiadomość</label>
             <textarea
               id="form__message"
+              value={message}
               onChange={(e) => {
                 setMessage(e.target.value);
               }}
@@ -191,7 +102,10 @@ export function Contact() {
               Wiadomość musi mieć conajmniej 120 znaków!
             </span>
           </div>
-          <button className="btn__submit">Wyślij</button>
+          <button className="btn__submit">
+            Wyślij
+            {spinner === 1 ? <LinearIndeterminate /> : ""}
+          </button>
         </form>
       </div>
     </div>
