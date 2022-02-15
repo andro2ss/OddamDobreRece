8e185e27f46149b78f7d20c60b0a710a84691002
andro2ss <a.brzeski@hotmail.com>
2022-02-15 15:40:24 -0800

    added posibility to add extra class to component

--- src/components/common/buttons/Button_OddajRzecz.js
+++ src/components/common/buttons/Button_OddajRzecz.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import React from "react";
 
-export function Button_OddajRzecz() {
+export function Button_OddajRzecz({ extraClass }) {
+  if (!extraClass) extraClass = "";
+  let tempClassName = "btn__link " + extraClass;
   return (
-    <Link to="logowanie" className="btn__link">
-      ODDAJ <br />
-      RZECZY
+    <Link to="logowanie" className={tempClassName}>
+      <span>ODDAJ</span>
+      <span>RZECZY</span>
     </Link>
   );
 }
