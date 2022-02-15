90974d6c6da2c4fc21e45c13ed39987698b09dec
andro2ss <a.brzeski@hotmail.com>
2022-02-15 15:37:50 -0800

    created register function

---
+++ src/functions/accountSection/accountRegisterUser.js
@@ -0,0 +1,11 @@
+import { addDoc, collection } from "firebase/firestore";
+import db from "../../firebase";
+
+export const createNewUser = async (login, password, setLoadedUsers) => {
+  const collectionRef = collection(db, "users");
+  const payload = { email: login, password: password };
+  const docRef = await addDoc(collectionRef, payload);
+  console.log("The new ID is: " + docRef.id);
+  localStorage.setItem("logUser", JSON.stringify(payload));
+  window.location.href = "../";
+};
