df15914323d2ae660053a2145d00643becc800b6
andro2ss <a.brzeski@hotmail.com>
2022-02-15 23:38:23 -0800

    renamed started building layout

---
+++ src/components/OddajRzeczy.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Contact } from "./home/Sections/Contact";
+
+function OddajRzeczy(props) {
+  return (
+    <div>
+      <Contact />
+    </div>
+  );
+}
+
+export default OddajRzeczy;
