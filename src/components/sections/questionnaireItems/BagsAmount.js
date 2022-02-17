6707bf7f7d478acb9fa09939887d32f5a6014f68
andro2ss <a.brzeski@hotmail.com>
2022-02-17 07:34:58 -0800

    created component for 2 step

---
+++ src/components/sections/questionnaireItems/BagsAmount.js
@@ -0,0 +1,18 @@
+import React from "react";
+import SelectInput from "../../common/SelectInput";
+
+export function BagsAmount({ value, setValue }) {
+  const sData = {
+    name: "bagsAmount",
+    values: ["1", "2", "3", "4", "5"],
+  };
+  return (
+    <div className="BagsAmount">
+      <h3>Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:</h3>
+      <div className="select__container">
+        <label>Liczba 60l worków:</label>
+        <SelectInput selectData={sData} value={value} setValue={setValue} />
+      </div>
+    </div>
+  );
+}
