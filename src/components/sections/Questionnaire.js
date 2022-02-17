c2b4adca8709e6ce690b7287d53cdbfaacb0258a
andro2ss <a.brzeski@hotmail.com>
2022-02-17 07:37:43 -0800

    progress backup - moved components to separate files

--- src/components/sections/Questionnaire.js
+++ src/components/sections/Questionnaire.js
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { ItemsToGive } from "./questionnaireItems/ItemsToGive";
+import { BagsAmount } from "./questionnaireItems/BagsAmount";
+import stepHandle from "../../functions/questionnaireSection/stepHandle";
+import { Location } from "./questionnaireItems/Location";
+import { Kurier } from "./questionnaireItems/Kurier";
 
 function Questionnaire({ setInfoBarText }) {
   const [step, setStep] = useState(1);
   const [sectionBody, setSectionBody] = useState(<>init</>);
+  const [step1Value, setStep1Value] = useState("init");
+  const [step2Value, setStep2Value] = useState(0);
+  const [step3ValueCity, setStep3ValueCity] = useState(0);
 
   useEffect(() => {
     switch (step) {
@@ -11,33 +19,7 @@ function Questionnaire({ setInfoBarText }) {
           "Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać."
         );
         setSectionBody(
-          <div>
-            <h3>Zaznacz co chcesz oddać:</h3>
-            <div className="checkbox__container">
-              <div className="flex">
-                <input type="radio" id="ubrania1" name="ubrania1" />
-                <label htmlFor="ubrania1">
-                  ubrania, które nadają się do ponownego użycia
-                </label>
-              </div>
-              <div className="flex">
-                <input type="radio" id="ubrania2" name="ubrania2" />
-                <label htmlFor="ubrania2">ubrania, do wyrzucenia</label>
-              </div>
-              <div className="flex">
-                <input type="radio" id="zabawki" name="zabawki" />
-                <label htmlFor="zabawki">zabawki</label>
-              </div>
-              <div className="flex">
-                <input type="radio" id="ksiazki" name="ksiazki" />
-                <label htmlFor="ksiazki">książki</label>
-              </div>
-              <div className="flex">
-                <input type="radio" id="inne" name="inne" />
-                <label htmlFor="inne">Inne</label>
-              </div>
-            </div>
-          </div>
+          <ItemsToGive setValue={setStep1Value} value={step1Value} />
         );
         break;
       case 2:
@@ -45,20 +27,7 @@ function Questionnaire({ setInfoBarText }) {
           "Wszystkie rzeczy do oddania zapakuj w 60l worki. Dokładną instrukcję jak poprawnie spakować rzeczy znajdziesz TUTAJ."
         );
         setSectionBody(
-          <div>
-            <h3>Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:</h3>
-            <div>
-              <label htmlFor="liczbaWorkow">Liczba 60l worków:</label>
-              <select name="liczbaWorkow" id="liczbaWorkow">
-                <option value="">--wybierz--</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-              </select>
-            </div>
-          </div>
+          <BagsAmount value={step2Value} setValue={setStep2Value} />
         );
         break;
       case 3:
@@ -66,126 +35,61 @@ function Questionnaire({ setInfoBarText }) {
           "Jeśli wiesz komu chcesz pomóc, możesz wpisać nazwę tej organizacji w wyszukiwarce. Możesz też filtrować organizacje po ich lokalizacji bądź celu ich pomocy."
         );
         setSectionBody(
-          <div>
-            <div>
-              <label htmlFor="liczbaWorkow">
-                <h3>Lokalizacja:</h3>
-              </label>
-              <select name="lokalizacja" id="lokalizacja">
-                <option value="">--wybierz--</option>
-                <option value="Poznań">Poznań</option>
-                <option value="Warszawa">Warszawa</option>
-                <option value="Kraków">Kraków</option>
-                <option value="Wrocław">Wrocław</option>
-                <option value="Katowice">Katowice</option>
-              </select>
-            </div>
-            <div>
-              <h4>Komu chcesz pomóc?</h4>
-              <box>
-                <button>dzieciom</button>
-                <button>samotnym matkom</button>
-                <button>bezdomnym</button>
-                <button>niepełnosprawnym</button>
-                <button>osobom starszym</button>
-              </box>
-            </div>
-            <div>
-              <label htmlFor="wybranaOrganizacja">
-                <h4>Wpisz nazwę konkretnej organizacji (opcjonalnie)</h4>
-              </label>
-              <input
-                type="text"
-                id="wybranaOrganizacja"
-                name="wybranaOrganizacja"
-              />
-            </div>
-          </div>
+          <Location city={step3ValueCity} setCity={setStep3ValueCity} />
         );
         break;
       case 4:
         setInfoBarText("Podaj adres oraz termin odbioru rzeczy.");
-        setSectionBody(
-          <div>
-            <h3>Podaj adres oraz termin odbioru rzecz przez kuriera</h3>
-            <box>
-              <div>
-                <h4>Adres odbioru:</h4>
-                <div className="flex">
-                  <label htmlFor="ulica">Ulica</label>
-                  <input type="text" id="ulica" name="ulica" />
-                </div>
-                <div className="flex">
-                  <label htmlFor="miasto">Miasto</label>
-                  <input type="text" id="miasto" name="miasto" />
-                </div>
-                <div className="flex">
-                  <label htmlFor="kodPocztowy">Kod pocztowy</label>
-                  <input type="text" id="kodPocztowy" name="kodPocztowy" />
-                </div>
-                <div className="flex">
-                  <label htmlFor="nrTel">Numer telefonu</label>
-                  <input type="text" id="nrTel" name="nrTel" />
-                </div>
-              </div>
-              <div>
-                <h4>Termin odbioru:</h4>
-                <div>
-                  <label htmlFor="data">Data</label>
-                  <input type="text" id="data" name="data" />
-                </div>
-                <div>
-                  <label htmlFor="godzina">Godzina</label>
-                  <input type="text" id="godzina" name="godzina" />
-                </div>
-                <div>
-                  <label htmlFor="uwagiKurier">Uwagi dla kuriera</label>
-                  <textarea
-                    id="uwagiKurier"
-                    name="uwagiKurier"
-                    rows="4"
-                    cols="50"
-                  />
-                </div>
-              </div>
-            </box>
-          </div>
-        );
+        setSectionBody(<Kurier />);
+        break;
+      case 5:
+        setInfoBarText("ale urwał");
+        setSectionBody(<div>Podsumowanko</div>);
+        break;
+      case 6:
+        setInfoBarText("danke");
+        setSectionBody(<div>dziekowka</div>);
         break;
     }
-  }, [step]);
-
-  const stepHandle = (valueStep) => {
-    let tempStep = step;
-    tempStep += valueStep;
-    if (tempStep < 1) {
-      tempStep = 1;
-    } else if (tempStep > 4) {
-      tempStep = 4;
-    }
-    setStep(tempStep);
-  };
+    console.log(step1Value);
+    console.log(step2Value);
+    console.log(step3ValueCity);
+  }, [step, step1Value, step2Value, step3ValueCity]);
 
   return (
     <section id="questionnaire">
       <span className="stepCounter">Krok {step}/4</span>
       <div>{sectionBody}</div>
-      <box className="btn__container">
-        {step === 1 ? (
+      <div className="btn__container">
+        {step === 1 || step === 6 ? (
           ""
         ) : (
-          <button className="btn__stepChange" onClick={() => stepHandle(-1)}>
+          <button
+            className="btn__stepChange"
+            onClick={() => stepHandle(-1, setStep, step)}
+          >
             Wstecz
           </button>
         )}
-        {step === 4 ? (
+        {step === 6 ? (
           ""
         ) : (
-          <button className="btn__stepChange" onClick={() => stepHandle(1)}>
-            Dalej
+          <button
+            className="btn__stepChange"
+            onClick={() => {
+              if (step === 1 && step1Value === "init") {
+                alert("Aby przejść do kolejnego kroku wybierz co chcesz oddać");
+              } else if (step === 2 && step2Value === 0) {
+                alert("Podaj liczbę worków, w które spakowano rzeczy");
+              } else {
+                stepHandle(1, setStep, step);
+              }
+            }}
+          >
+            {step === 5 ? "Potwierdzam" : "Dalej"}
           </button>
         )}
-      </box>
+      </div>
     </section>
   );
 }
