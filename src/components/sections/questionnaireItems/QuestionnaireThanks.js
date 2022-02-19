6dab136ab8223312b7d3281931a4016bd2e12b68
andro2ss <a.brzeski@hotmail.com>
2022-02-19 12:15:55 -0800

    added reset data

--- src/components/sections/questionnaireItems/QuestionnaireThanks.js
+++ src/components/sections/questionnaireItems/QuestionnaireThanks.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Decoration } from "../../common/decorations/Decoration";
+import { useDispatch } from "react-redux";
+import { step1NewValue } from "../../../actions";
 
-function QuestionnaireThanks(props) {
+function QuestionnaireThanks({
+  setData1,
+  setData2,
+  setData3,
+  setData4,
+  setData5,
+}) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(step1NewValue("init"));
+    setData1(0);
+    setData2(0);
+    setData3([]);
+    setData4("");
+    setData5({
+      street: "",
+      city: "",
+      postCode: "",
+      telephone: "",
+      data: "",
+      hour: "",
+      notes: "",
+    });
+  }, []);
   return (
     <div className="questionnaireThanks">
       <span>
