cf9785f42825005477c5c17093a82327b864262c
andro2ss <a.brzeski@hotmail.com>
2022-02-18 16:23:12 -0800

    added redux actions

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+export const step1NewValue = (value) => {
+  return {
+    type: "STEP1NEWVALUE",
+    payload: value,
+  };
+};
+export const setQuestioSum = (value) => {
+  return {
+    type: "SETSUMMARY",
+    payload: value,
+  };
+};
