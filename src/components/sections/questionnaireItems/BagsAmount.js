221a166e07a459b851843589b3b696aa2df09d87
andro2ss <a.brzeski@hotmail.com>
2022-02-18 16:22:11 -0800

    refactored place and fixed className

--- src/components/sections/questionnaireItems/BagsAmount.js
+++ src/components/sections/questionnaireItems/BagsAmount.js
@@ -1,5 +1,5 @@
 import React from "react";
-import SelectInput from "../../common/SelectInput";
+import SelectInput from "../../common/inputs/SelectInput";
 
 export function BagsAmount({ value, setValue }) {
   const sData = {
@@ -7,7 +7,7 @@ export function BagsAmount({ value, setValue }) {
     values: ["1", "2", "3", "4", "5"],
   };
   return (
-    <div className="BagsAmount">
+    <div className="bagsAmount">
       <h3>Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:</h3>
       <div className="select__container">
         <label>Liczba 60l worków:</label>
