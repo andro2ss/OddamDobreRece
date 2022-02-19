f79b00770ce3d3604c858e5d4af2a36d260d0434
andro2ss <a.brzeski@hotmail.com>
2022-02-18 16:23:51 -0800

    added redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style/main.scss";
 import App from "./App";
+import { compose, createStore } from "redux";
+import allReducer from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  allReducer,
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
